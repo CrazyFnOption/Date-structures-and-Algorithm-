9f45bc43d36b7a58e0fb3071c5d28961c4934263
墨橹 <wangshuxiao@wangshuxiao.local>
2018-10-13 15:12:41 +0800

    Initial Commit

---
+++ 邮件管理系统/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  邮件管理系统
+//
+//  Created by 王舒啸 on 2018/10/13.
+//  Copyright © 2018 王舒啸. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
